35237f9c9cb3e4a00feb453200aaaf4e08d2cc3e
olaszm <martin1olasz@gmail.com>
2021-03-14 14:55:41 +0000

    update:aded index & details view + bootsrap

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse,Http404
+from django.urls import reverse
+from django.views import generic
+import json
 
-# Create your views here.
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {'question' : question, 'error_message':"You didn't select a choice"})
+    else: 
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
